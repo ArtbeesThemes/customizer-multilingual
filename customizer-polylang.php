fd2d2b360e4dc561a9bf4774644a0c8943b7b88e
Hector <6560172+h71@users.noreply.github.com>
2018-10-18 10:37:58 +0330

    Add support for WPML

--- customizer-polylang.php
+++ customizer-polylang.php
@@ -1,38 +1,147 @@
 <?php
 /**
  * If Polylang is active:
- * - save and retrieve customizer setting per language
- * - on front-page, set options and theme mod for the selected lamguage
+ * - save and retrieve customizer setting per language.
+ * - on front-page, set options and theme mod for the selected language.
  *
  * Inspired by https://github.com/fastlinemedia/customizer-export-import
+ *
+ * @since 1.0.0
  */
-namespace Soderlind\Customizer\Polylang; // replace with your namespace
+namespace JupiterX\Customizer\Multilingual;
 
-if ( ! \function_exists( 'pll_current_language' ) ) {
-	return; // Polylang isn't activated, bail out
+if ( ! \function_exists( 'pll_current_language' ) && ! \class_exists( 'SitePress' ) ) {
+	return;
 }
 
-\add_action( 'customize_save_after'              , __NAMESPACE__ . '\CustomizerPolylang::save_settings', 1000 );
-\add_action( 'plugins_loaded'                    , __NAMESPACE__ . '\CustomizerPolylang::load_settings', 9 ); // Must happen before 10 when _wp_customize_include() fires.
-\add_action( 'after_setup_theme'                 , __NAMESPACE__ . '\CustomizerPolylang::load_settings' );
-\add_action( 'customize_controls_enqueue_scripts', __NAMESPACE__ . '\CustomizerPolylang::add_lang_to_customizer_previewer', 9 );
-\add_action( 'wp_before_admin_bar_render'        , __NAMESPACE__ . '\CustomizerPolylang::on_wp_before_admin_bar_render', 100 );
-\add_action( 'admin_menu'                        , __NAMESPACE__ . '\CustomizerPolylang::on_admin_menu', 100 );
+\add_action( 'customize_save_after'              , __NAMESPACE__ . '\CustomizerMultilingual::save_settings', 1000 );
+\add_action( 'plugins_loaded'                    , __NAMESPACE__ . '\CustomizerMultilingual::load_settings', 9 ); // Must happen before 10 when _wp_customize_include() fires.
+\add_action( 'after_setup_theme'                 , __NAMESPACE__ . '\CustomizerMultilingual::load_settings' );
+\add_action( 'customize_controls_enqueue_scripts', __NAMESPACE__ . '\CustomizerMultilingual::add_lang_to_customizer_previewer', 9 );
+\add_action( 'wp_before_admin_bar_render'        , __NAMESPACE__ . '\CustomizerMultilingual::on_wp_before_admin_bar_render', 100 );
+\add_action( 'admin_menu'                        , __NAMESPACE__ . '\CustomizerMultilingual::on_admin_menu', 100 );
+\add_action( 'after_setup_theme'                 , __NAMESPACE__ . '\CustomizerMultilingual::remove_filters', 5 );
 
 
-interface CustimizerPolylangInterface {
+interface CustimizerMultilingualInterface {
 	public static function save_settings( $wp_customize);
 	public static function load_settings( $wp_customize = null);
 }
 
-class CustomizerPolylang implements CustimizerPolylangInterface {
+class CustomizerMultilingual implements CustimizerMultilingualInterface {
 
-	public static function save_settings( $wp_customize ) {
-		if ( ! function_exists( 'pll_current_language' ) ) {
-			return;
+	/**
+	 * Remove bloginfo update filters. As we save options per language in this class, we don't need WPML functionality for the .
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public static function remove_filters() {
+		global $WPML_String_Translation;
+		remove_filter( 'pre_update_option_blogname', [ $WPML_String_Translation, 'pre_update_option_blogname' ], 5 );
+		remove_filter( 'pre_update_option_blogdescription', [ $WPML_String_Translation, 'pre_update_option_blogdescription' ], 5 );
+	}
+
+	/**
+	 * Get current language.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string|bool $language|false Current language or false when none of Polylang & WPML are active.
+	 */
+	public static function get_language() {
+		if ( function_exists( 'pll_current_language' ) ) {
+			$language = pll_current_language();
+
+			if ( ! $language ) {
+				$language = pll_default_language();
+			}
+
+			return $language;
 		}
 
-		$language = pll_current_language();
+		if ( class_exists('SitePress') && defined( 'ICL_LANGUAGE_CODE' ) ) {
+			return ICL_LANGUAGE_CODE;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get a list of active languages with extra parameters like name and slug.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array|bool $languages|false List of active languages or false when none of Polylang & WPML are active.
+	 */
+	public static function get_languages_list() {
+		if ( function_exists( 'pll_current_language' ) ) {
+			return get_option( '_transient_pll_languages_list' );
+		}
+
+		if ( class_exists('SitePress') && defined( 'ICL_LANGUAGE_CODE' ) ) {
+			$list = icl_get_languages('skip_missing=1');
+			$languages = [];
+
+			foreach ( $list as $language ) {
+				$temp = [];
+				$temp['name'] = $language['native_name'];
+				$temp['slug'] = $language['code'];
+				$languages[] = $temp;
+			}
+
+			return $languages;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get a proper option key per plugin.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string|bool Option key or false when none of Polylang & WPML are active.
+	 */
+	public static function get_option_key() {
+		if ( function_exists( 'pll_current_language' ) ) {
+			return '_customizer_polylang_settings_';
+		}
+
+		if ( class_exists('SitePress') && defined( 'ICL_LANGUAGE_CODE' ) ) {
+			return '_customizer_wpml_settings_';
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get home URL of current language.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string|bool Home URL of current language or false when none of Polylang & WPML are active.
+	 */
+	public static function get_home_url( $language ) {
+		if ( function_exists( 'pll_current_language' ) ) {
+			return pll_home_url( $language );
+		}
+
+		if ( class_exists('SitePress') ) {
+			global $sitepress;
+			return $sitepress->language_url( $language );
+		}
+
+		return false;
+	}
+
+	public static function save_settings( $wp_customize ) {
+		$language = self::get_language();
+
+		if ( ! $language ) {
+			return;
+		}
 
 		$theme    = get_stylesheet();
 		$template = get_template();
@@ -48,7 +157,7 @@ class CustomizerPolylang implements CustimizerPolylangInterface {
 		foreach ( $settings as $key => $setting ) {
 			if ( 'option' == $setting->type ) {
 				switch ( $key ) {
-					// icnore these
+					// ignore these
 					case stristr( $key, 'widget_' ):
 					case stristr( $key, 'sidebars_' ):
 					case stristr( $key, 'nav_menus_' ):
@@ -68,44 +177,47 @@ class CustomizerPolylang implements CustimizerPolylangInterface {
 				$data['options'][ $option_key ] = $option_value;
 			}
 		}
+
 		if ( function_exists( 'wp_get_custom_css_post' ) ) {
 			$data['wp_css'] = wp_get_custom_css();
 		}
 
 		$option_prefix = \str_replace( '-', '_', $template );
-		\update_option( $option_prefix . '_customizer_polylang_settings_' . $language, $data );
+		\update_option( $option_prefix . self::get_option_key() . $language, $data );
 	}
 
 	public static function load_settings( $wp_customize = null ) {
 		global $cei_error;
 
-		if ( ! function_exists( 'pll_current_language' ) ) {
+		$language = self::get_language();
+
+		if ( ! $language ) {
 			return;
 		}
 
-		$language      = pll_current_language();
 		$template      = get_template();
 		$option_prefix = \str_replace( '-', '_', $template );
-		$data          = get_option( $option_prefix . '_customizer_polylang_settings_' . $language, false );
+		$data          = get_option( $option_prefix . self::get_option_key() . $language, false );
 
 		if ( $data ) {
 			// Data checks.
 			if ( 'array' != gettype( $data ) ) {
 				return;
 			}
+
 			if ( ! isset( $data['template'] ) || ! isset( $data['mods'] ) ) {
 				return;
 			}
+
 			if ( $data['template'] != $template ) {
 				return;
 			}
 
 			// Import custom options.
 			if ( isset( $data['options'] ) ) {
-
 				foreach ( $data['options'] as $option_key => $option_value ) {
-					if ( \class_exists( 'CustomizerPolylangOption' ) ) {
-						$option = new CustomizerPolylangOption(
+					if ( \class_exists( 'Customizermultilingialoption' ) ) {
+						$option = new Customizermultilingialoption(
 							$wp_customize, $option_key, [
 								'default'    => '',
 								'type'       => 'option',
@@ -114,7 +226,7 @@ class CustomizerPolylang implements CustimizerPolylangInterface {
 						);
 						$option->import( $option_value );
 					} else {
-						update_option( $option_key, $option_value );
+						\update_option( $option_key, $option_value );
 					}
 				}
 			}
@@ -134,40 +246,50 @@ class CustomizerPolylang implements CustimizerPolylangInterface {
 	 * @author soderlind
 	 * @version 1.0.0
 	 * @link https://gist.github.com/soderlind/1908634f5eb0c1f69428666dd2a291d0
+	 *
+	 * @since 1.0.0
 	 */
 	public static function add_lang_to_customizer_previewer() {
-		if ( function_exists( 'pll_current_language' ) ) {
-			$handle    = 'dss-add-lang-to-template';
-			$js_path_url = trailingslashit( apply_filters( 'scp_js_path_url', get_stylesheet_directory_uri() . '/js/' ) );
-			$src       = $js_path_url . '/customizer-polylang.js';
-			$deps      = [ 'customize-controls' ];
-			$version   = rand();
-			$in_footer = 1;
-			wp_enqueue_script( $handle, $src, $deps, $version, $in_footer );
-			$language = ( empty( $_REQUEST['lang'] ) ) ? pll_current_language() : $_REQUEST['lang'];
+		$languages = self::get_languages_list();
 
-			if ( empty( $language ) ) {
-				$language = pll_default_language();
-			}
-			$url = add_query_arg( 'lang', $language, pll_home_url( $language ) );
-
-			wp_add_inline_script(
-				$handle,
-				sprintf(
-					'PSPolyLang.init( %s );', wp_json_encode(
-						[
-							'url'              => $url,
-							'languages'        => get_option( '_transient_pll_languages_list' ),
-							'current_language' => $language,
-						]
-					)
-				), 'after'
-			);
+		if ( ! $languages ) {
+			return;
 		}
+
+		$handle    = 'dss-add-lang-to-template';
+		$js_path_url = trailingslashit( apply_filters( 'scp_js_path_url', get_stylesheet_directory_uri() . '/js/' ) );
+		$src       = $js_path_url . 'customizer-polylang.js';
+		$deps      = [ 'customize-controls' ];
+		$version   = rand();
+		$in_footer = 1;
+		wp_enqueue_script( $handle, $src, $deps, $version, $in_footer );
+		$language = ( empty( $_REQUEST['lang'] ) ) ? self::get_language() : $_REQUEST['lang'];
+
+		if ( empty( $language ) ) {
+			$language = self::default_language();
+		}
+
+		$url = add_query_arg( 'lang', $language, self::get_home_url( $language ) );
+
+		wp_add_inline_script(
+			$handle,
+			sprintf(
+				'JupiterCustomizerMultilingual.init( %s );', wp_json_encode(
+					[
+						'url'              => $url,
+						'languages'        => $languages,
+						'current_language' => $language,
+						'switcher_text'    => __( 'Language:', 'jupiterx' ),
+					]
+				)
+			), 'after'
+		);
 	}
 
 	/**
 	 * Append lang="contrycode" to the customizer url in the adminbar
+	 *
+	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
@@ -175,13 +297,15 @@ class CustomizerPolylang implements CustimizerPolylangInterface {
 		global $wp_admin_bar;
 		$customize_node = $wp_admin_bar->get_node( 'customize' );
 		if ( ! empty( $customize_node ) ) {
-			$customize_node->href = add_query_arg( 'lang', pll_current_language(), $customize_node->href );
+			$customize_node->href = add_query_arg( 'lang', self::get_language(), $customize_node->href );
 			$wp_admin_bar->add_node( $customize_node );
 		}
 	}
 
 	/**
 	 * Append lang="contrycode" to the customizer url in the Admin->Apperance->Customize menu
+	 *
+	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
@@ -193,7 +317,7 @@ class CustomizerPolylang implements CustimizerPolylangInterface {
 		}
 		foreach ( $submenu[ $parent ] as $k => $d ) {
 			if ( 'customize' == $d['1'] ) {
-				$submenu[ $parent ][ $k ]['2'] = add_query_arg( 'lang', pll_current_language(), $submenu[ $parent ][ $k ]['2'] );
+				$submenu[ $parent ][ $k ]['2'] = add_query_arg( 'lang', self::get_language(), $submenu[ $parent ][ $k ]['2'] );
 				break;
 			}
 		}
@@ -208,7 +332,7 @@ if ( class_exists( 'WP_Customize_Setting' ) ) {
 	 *
 	 * @since 0.3
 	 */
-	final class CustomizerPolylangOption extends \WP_Customize_Setting {
+	final class Customizermultilingialoption extends \WP_Customize_Setting {
 
 
 		/**
